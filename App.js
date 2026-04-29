5d4a0c06596b5aca97c991791623a6f0114ed876
PreciousC26git <dubemprecious3@gmail.com>
2026-04-29 19:19:57 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+
+
+
+const welcome = require("./message")
+
+//named exports
+const {multiply, divide} = require("./calculator")
+
+hi()
+console.log(multiply(5, 3))
+console.log(divide(12, 4))
---
+++ message.js
@@ -0,0 +1,6 @@
+
+function welcome () {
+    console.log("hello to today class, Okoli Precious!")
+}
+
+module.exports = welcome
